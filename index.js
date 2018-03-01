836686777214fc95e1c6ac9565b9c01658100ea8
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-01 02:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,4 @@ function sayHiToGrandma(string) {
   else if ((string)===lowercase) {
     return "I can't hear you!"
   }
-  else if (string) === "I love you, Grandma.") {
-    return "I love you, too."
-  }
 }
